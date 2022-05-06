591d946a4c0310568ed6cfc3d67cf4185ebd7f56
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-06 15:02:01 -0600

    Repurposed many if/else if loops to concatenate various arrays
    Experimenting with a button function that would incorporate the various prompt choices

--- Develop/script.js
+++ Develop/script.js
@@ -88,7 +88,8 @@ var special =
 "?"]
 
 var numbers = 
-["1",
+["0",
+"1",
 "2",
 "3",
 "4",
@@ -98,95 +99,114 @@ var numbers =
 "8",
 "9"]
 
+var confirmPassBoth = 
+[];
+var confirmPass =
+[];
+var confirmSpec =
+[];
+var confirmNum =
+[];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var min = 8;
-var max = 60;
-  var validatePasslength = function() {
-    var lengthChoice = window.prompt("Enter desired password length between 8 - 60");
-        if (lengthChoice === null) {
-        console.log("null")
-        return;
-      } else if (lengthChoice < min) {
-        window.alert("Invalid Entry; Entry too small");
-        validatePasslength();
-      } else if (lengthChoice > max) {
-        window.alert("Invalid Entry; Entry too large");
-        validatePasslength();
-      } else {
-        console.log("You've entered " + lengthChoice);
-      }
-      return lengthChoice
-  }
 
-  var validateChars = function() {
-    var charChoice = window.prompt("Use both lowercase and uppercase characters? ('yes' or 'no')");
+// Function for determining password length
+  var min = 8;
+  var max = 128;
+    var validatePasslength = function() {
+      var lengthChoice = window.prompt("Enter desired password length between 8 - 128");
+          if (lengthChoice === null) {
+          console.log("null")
+          return;
+        } else if (lengthChoice < min) {
+          window.alert("Invalid Entry; Entry too small");
+          validatePasslength();
+        } else if (lengthChoice > max) {
+          window.alert("Invalid Entry; Entry too large");
+          validatePasslength();
+        } else {
+          console.log("You've entered " + lengthChoice);
+        }
+        return lengthChoice
+    }
+
+// Funtion for determining whether to use both uppercase and lowercase letters, or one of the two
+    var validateChars = function() {
+      var charChoice = window.prompt("Use both lowercase and uppercase characters? ('yes' or 'no')");
+          if (charChoice === null) {
+          console.log("null")
+          return;
+        } else if (charChoice === "yes") {
+          confirmPass = upperCharacters.concat(lowerCharacters);
+          console.log(confirmPass)
+        } else if (charChoice === "no") {
+          upLowChoice();
+        } else {
+          window.alert("Invalid Entry; Input must be 'yes' or 'no'");
+          validateChars();
+        }
+        return charChoice
+    }
+
+// As previously stated, function for determining whether to use upper, or lowercase letters, in the event using both is declined
+    var upLowChoice = function() {
+      var charChoice = window.prompt("Use only lowercase or uppercase letters? (Enter 'upper' or 'lower')");
         if (charChoice === null) {
         console.log("null")
         return;
-      } else if (charChoice === "yes") {
-        var confirmPass = upperCharacters.concat(lowerCharacters);
-        console.log(confirmPass)
-      } else if (charChoice === "no") {
-        upLowChoice();
+      } else if (charChoice === "upper") {
+        confirmPass = upperCharacters;
+        console.log(confirmPass);
+      } else if (charChoice === "lower") {
+        confirmPass = lowerCharacters;
+        console.log(confirmPass);
       } else {
-        window.alert("Invalid Entry; Input must be 'yes' or 'no'");
-        validateChars();
+        window.alert("Invalid Entry; Input must be 'upper' or 'lower'");
+        upLowChoice();
       }
       return charChoice
-  }
-
-  var upLowChoice = function() {
-    var charChoice = window.prompt("Use only lowercase or uppercase letters? (Enter 'upper' or 'lower')");
-      if (charChoice === null) {
-      console.log("null")
-      return;
-    } else if (charChoice === "upper") {
-      var confirmPass = upperCharacters;
-      console.log(confirmPass)
-    } else if (charChoice === "lower") {
-      var confirmPass = lowerCharacters;
-      console.log(confirmPass)
-    } else {
-      window.alert("Invalid Entry; Input must be 'upper' or 'lower'");
-      upLowChoice();
     }
-    return charChoice
-  }
 
-  var useSpecial = function() {
-    var specialChoice = window.prompt("Use special characters? ('yes or no')", "yes");
-      if (specialChoice === null) {
-      console.log("null")
-      return;
-    } else if (specialChoice === "yes") {
-      console.log(charChoice)
-      console.log("Use special characters.");
-    } else if (specialChoice === "no") {
-      console.log("Do not use special characters.");
-    } else {
-      window.alert("Invalid Entry; Input must be 'yes' or 'no'");
-      useSpecial();
+// Function for determining whether or not to add special characters 
+    var useSpecial = function() {
+      var specialChoice = window.prompt("Use special characters? ('yes or no')", "yes");
+        if (specialChoice === null) {
+        console.log("null")
+        return;
+      } else if (specialChoice === "yes") {
+        console.log("Use special characters.");
+        confirmSpec = confirmPass.concat(special);
+        console.log(confirmSpec);
+      } else if (specialChoice === "no") {
+        console.log("Do not use special characters.");
+      } else {
+        window.alert("Invalid Entry; Input must be 'yes' or 'no'");
+        useSpecial();
+      }
+      return specialChoice
     }
-    return specialChoice
-  }
 
-  var useNumbers = function() {
-    var numChoice = window.prompt("Use numbers? ('yes' or 'no')", "yes");
-      if (numChoice === null) {
-      console.log("null")
-      return;
-    } else if (numChoice === "yes") {
-      console.log("Use numbers.");
-    } else if (numChoice === "no") {
-      console.log("Do not use numbers.");
-    } else {
-      window.alert("Invalid Entry; Input must be 'yes' or 'no'");
-      useNumbers();
+// Function for determining whether or not to add numbers
+    var useNumbers = function() {
+      var numChoice = window.prompt("Use numbers? ('yes' or 'no')", "yes");
+        if (numChoice === null) {
+        console.log("null")
+        return;
+      } else if (numChoice === "yes") {
+        console.log("Use numbers.");
+        confirmNum = confirmSpec.concat(numbers);
+        console.log(confirmNum);
+      } else if (numChoice === "no") {
+        console.log("Do not use numbers.");
+      } else {
+        window.alert("Invalid Entry; Input must be 'yes' or 'no'");
+        useNumbers();
+      }
+      return numChoice
     }
-    return numChoice
-  }
 
 // Write password to the #password input
 function writePassword() {
@@ -202,4 +222,12 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", () => {
+  let letters = charChoice;
+    if (specialChoice === "yes" && numChoice === "yes")
+      letters += numbers, special;
+    else if (specialChoice === "yes" && numChoice === "no")
+      letters += special;
+    else if (specialChoice === "no" && numChoice === "yes")
+      letters += numbers;
+});
